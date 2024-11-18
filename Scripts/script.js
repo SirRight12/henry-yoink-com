69b732d5a51643b31b31faf099b9e9981189e784
Deltaripoff <122371988+Deltaripoff@users.noreply.github.com>
2024-11-18 14:18:46 -0600

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -813,56 +813,56 @@ function Tales() {
                 audio.remove()
                 audio = new Audio('Music/Block Tales OST_Greed.mp3')
                 audio.loop = true
-                audio.play
+                audio.play()
                 break
             case 'Chocolate':
                 audio.pause()
                 audio.remove()
                 audio = new Audio('Music/Block Tales OST_Bigfoot.mp3')
                 audio.loop = true
-                audio.play
+                audio.play()
                 break
             case 'Coal':
                 audio.pause()
                 audio.remove()
                 audio = new Audio('Music/Block Tales OST_Slasher.mp3')
                 audio.loop = true
-                audio.play
+                audio.play()
                 break
             case 'Gold':
                 audio.pause()
                 audio.remove()
                 audio = new Audio('Music/Block Tales OST_Turkey.mp3')
                 audio.loop = true
-                audio.play
+                audio.play()
                 break
             case 'Lime':
                 audio.pause()
                 audio.remove()
                 audio = new Audio('Music/Block Tales OST_Green & Purple Noobs.mp3')
                 audio.loop = true
-                audio.play
+                audio.play()
                 break
             case 'Orange':
                 audio.pause()
                 audio.remove()
                 audio = new Audio('Music/Block Tales OST_Bizville.mp3')
                 audio.loop = true
-                audio.play
+                audio.play()
                 break
             case 'Platinum':
                 audio.pause()
                 audio.remove()
                 audio = new Audio('Music/Block Tales OST_Solitude')
                 audio.loop = true
-                audio.play
+                audio.play()
                 break
             case 'Rose':
                 audio.pause()
                 audio.remove()
                 audio = new Audio('Music/Block Tales OST_Bubonic Plant.mp3')
                 audio.loop = true
-                audio.play
+                audio.play()
                 break
         }
     }
